54c54debe63f3d55a9fae5cdb4ca46b4e059391a
Rahul R <rraju@datainnovations.com>
2023-07-17 11:39:18 +0530

    Added Logout feature

--- TodoApp/Views/ProfileView.swift
+++ TodoApp/Views/ProfileView.swift
@@ -17,42 +17,54 @@ struct ProfileView: View {
     var body: some View {
         NavigationView{
             VStack{
-                // Avatar
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(Color.blue)
-                    .frame(width: 125, height: 125 )
-                    .padding()
-                // Info: Name, Email, Member since
-                VStack(alignment: .leading ){
-                    HStack{
-                        Text("Name: ")
-                            .bold()
-                        Text("Kailash")
-                    }.padding()
-                    HStack{
-                        Text("Email: ")
-                            .bold()
-                        Text("kailash@gmail.com")
-                    }.padding()
-                    HStack{
-                        Text("Member Since: ")
-                            .bold()
-                        Text("2023")
-                    }.padding() 
-                }.padding()
-                // Sign out
-                Button("Logout") {
-                    viewModel.logOut()
-                }.tint(.red)
-                    .padding()
-                
+                if let user = viewModel.user{
+                    profile(user: user)
+              }
+                else{
+                    Text("Loading Profile...")
+                }
             }
             .navigationTitle("Profile")
             
+        }.onAppear{
+            viewModel.fetchUser()
         }
     }
+    
+    @ViewBuilder
+    func profile(user:User) -> some View{
+        // Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125, height: 125 )
+            .padding()
+        // Info: Name, Email, Member since
+        VStack(alignment: .leading ){
+            HStack{
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }.padding()
+            HStack{
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }.padding()
+            HStack{
+                Text("Member Since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }.padding()
+        }.padding()
+        // Sign out
+        Button("Logout") {
+            viewModel.logOut()
+        }.tint(.red)
+        .padding()
+    }
+    
 }
 
 struct ProfileView_Previews: PreviewProvider {
